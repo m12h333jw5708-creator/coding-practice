e1e3de2f15052cf9a2a0e81abd8ebd59ca0aa513
jaewoo-park <m12h333jw5708@gmail.com>
2026-07-23 22:54:39 +0900

    bmi지수 구하는 코드
    책으로 배운 내용으로 구현했다. 아직 if문은 배우지 않았다

---
+++ BMICalculator.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	double height;
+	double weight;
+
+	printf("몸무게(kg)와 키(cm) 입력 : ");
+	scanf("%lf %lf", &weight, &height);
+	double bmi = weight / ((height / 100) * (height / 100));
+	((bmi >= 20.0) && (bmi < 25.0)) ? printf("표준입니다.") : printf("체중 관리가 필요합니다.");
+
+	return 0;
+}
