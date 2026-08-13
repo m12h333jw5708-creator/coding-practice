59b237bfb8710a76f84eb4003ed389b0cd5eb507
jaewoo-park <m12h333jw5708@gmail.com>
2026-08-13 21:53:12 +0900

    C로  up and down게임 구현

---
+++ up_down.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+
+	//1 - 100 사이 난수 생성
+	srand(time(NULL));
+	int num = rand() % 101;
+	
+	//유저 변수 선언
+	int user;
+	
+	//맞을 때까지 검사
+	while (1)
+	{
+		printf("Enter a number(1 - 100) : ");
+		scanf("%d", &user);
+
+		if (user == num)
+		{
+			printf("Congratulations. You got it right!!\n");
+			break;
+		}
+		else if (user < num)
+		{
+			printf("Up\n");
+		}
+		else
+		{
+			printf("down\n");
+		}
+	}
+	
+
+
+	return 0;
+}
